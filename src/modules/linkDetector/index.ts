ab2bb90a7040c09c92bd7c720f059e7ed6a2076d
angelopedroso <solonasusonly0@gmail.com>
2023-06-17 00:06:29 -0300

    perf: removed dupplicated code

--- src/modules/linkDetector/index.ts
+++ src/modules/linkDetector/index.ts
@@ -1,28 +1,35 @@
 import { ZapType } from '@modules/zapConstructor'
 import { isUrl, socialMediaRegex } from '@utils/globalVariable'
+import { db } from '@lib/auth/prisma-query'
+import { printError } from 'cli/terminal'
 
 const linkDetector = async ({ message, ...zap }: ZapType) => {
-  const chat = await zap.getChat()
+  try {
+    const user = await zap.getUser()
 
-  if (!chat.isGroup) return
+    const isAdmin = await zap.getUserIsAdmin(user.id._serialized)
 
-  const user = await zap.getUser()
+    if (isUrl.test(message!.body) && !isAdmin) {
+      const chat = await zap.getChat()
+      const groupInfo = await db.getGroupInfo(chat.id._serialized)
 
-  const isAdmin = await zap.getUserIsAdmin(user.id._serialized)
+      if (groupInfo.anti_link) {
+        const groupChat = await zap.getGroupChat()
+        const msgs = await chat.fetchMessages({ limit: 1, fromMe: false })
 
-  if (isUrl.test(message!.body) && !isAdmin) {
-    const groupChat = await zap.getGroupChat()
-    const msgs = await chat.fetchMessages({ limit: 1, fromMe: false })
+        const invalidLinks = msgs
+          .map((msg) => msg.links.filter((l) => !socialMediaRegex.test(l.link)))
+          .filter((links) => links.length > 0)
+          .flat()
 
-    const invalidLinks = msgs
-      .map((msg) => msg.links.filter((l) => !socialMediaRegex.test(l.link)))
-      .filter((links) => links.length > 0)
-      .flat()
-
-    if (invalidLinks.length !== 0) {
-      groupChat.removeParticipants([user.id._serialized])
-      message?.delete(true)
+        if (invalidLinks.length !== 0) {
+          groupChat.removeParticipants([user.id._serialized])
+          message?.delete(true)
+        }
+      }
     }
+  } catch (error: Error | any) {
+    printError('Link Detector: ' + error.message)
   }
 }
 
