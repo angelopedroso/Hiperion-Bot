3b04a932fc8dd7bebe4e088cfe5db9370cd9c2ed
angelopedroso <solonasusonly0@gmail.com>
2023-06-23 20:13:30 -0300

    fix: moved sendSticker and fixed some bugs

---
+++ src/modules/sticker/index.ts
@@ -0,0 +1,60 @@
+import { ZapType } from '@modules/zapConstructor'
+import { groupInfoCache } from '@typings/cache/groupInfo.interface'
+import { BOT_NAME } from '@utils/envs'
+
+import { MessageTypes } from 'whatsapp-web.js'
+
+async function sendSticker({ message, ...zap }: ZapType) {
+  const chat = await zap.getChat()
+  const validTypes = [MessageTypes.IMAGE, MessageTypes.VIDEO]
+
+  if (message?.hasMedia && validTypes.includes(message.type)) {
+    const media = await message.downloadMedia()
+
+    await chat.sendMessage(media, {
+      sendMediaAsSticker: true,
+      stickerAuthor: BOT_NAME,
+      stickerName: zap.translateMessage('fs.madeby', { name: BOT_NAME }),
+    })
+
+    return
+  }
+
+  if (message?.hasQuotedMsg) {
+    const quotedMsg = await message.getQuotedMessage()
+
+    if (quotedMsg.hasMedia && validTypes.includes(quotedMsg.type)) {
+      const media = await quotedMsg.downloadMedia()
+
+      chat.sendMessage(media, {
+        sendMediaAsSticker: true,
+        stickerAuthor: BOT_NAME,
+        stickerName: zap.translateMessage('fs.madeby', { name: BOT_NAME }),
+      })
+    }
+  }
+}
+
+async function sendAutoSticker(
+  { message, ...zap }: ZapType,
+  groupInfo: groupInfoCache | null | undefined,
+) {
+  const groupChat = await zap.getGroupChat()
+
+  if (!groupChat.isGroup || message?.body.includes('!fs')) return
+
+  if (message?.hasMedia && message.type !== MessageTypes.STICKER) {
+    if (groupInfo?.auto_sticker) {
+      const chat = await zap.getChat()
+      const media = await message.downloadMedia()
+
+      await chat.sendMessage(media, {
+        sendMediaAsSticker: true,
+        stickerAuthor: BOT_NAME,
+        stickerName: zap.translateMessage('fs.madeby', { name: BOT_NAME }),
+      })
+    }
+  }
+}
+
+export { sendSticker, sendAutoSticker }
