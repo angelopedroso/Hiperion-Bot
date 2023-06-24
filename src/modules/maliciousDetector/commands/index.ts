d9a31c6f82189558b216e4848203685c291bc081
angelopedroso <solonasusonly0@gmail.com>
2023-06-24 00:20:51 -0300

    feat: added command to change maliciousDetec on db

---
+++ src/modules/maliciousDetector/commands/index.ts
@@ -0,0 +1,33 @@
+import { db } from '@lib/auth/prisma-query'
+import { redis } from '@lib/prisma'
+import { ZapType } from '@modules/zapConstructor'
+
+export async function toggleMaliciousDetector({ message, ...zap }: ZapType) {
+  const chat = await zap.getGroupChat()
+  const user = await zap.getUser()
+
+  const isAdmin = await zap.getUserIsAdmin(user.id._serialized)
+
+  if (!isAdmin) return
+
+  if (chat.isGroup) {
+    const groupId = chat.id._serialized
+    const groupInfo = await db.getGroupInfo(groupId)
+
+    await db.updateGroupExceptParticipants(groupId, {
+      anti_porn: !groupInfo?.anti_porn,
+    })
+
+    if (groupInfo?.anti_porn) {
+      await message?.react('🟥')
+    } else {
+      await message?.react('🟩')
+    }
+
+    redis.del(`group-info:${groupId}`)
+
+    return
+  }
+
+  await message?.reply(zap.translateMessage('notgrouperror'))
+}
