881f0b9e475b9a95769282a6d1f6ed0bf50d0096
angelopedroso <solonasusonly0@gmail.com>
2023-06-24 00:19:53 -0300

    feat: added command to change autoinvite on db

---
+++ src/modules/groupInvite/commands/index.ts
@@ -0,0 +1,33 @@
+import { db } from '@lib/auth/prisma-query'
+import { redis } from '@lib/prisma'
+import { ZapType } from '@modules/zapConstructor'
+
+export async function toggleAutoInvite({ message, ...zap }: ZapType) {
+  const chat = await zap.getGroupChat()
+  const user = await zap.getUser()
+
+  const isAdmin = await zap.getUserIsAdmin(user.id._serialized)
+
+  if (!isAdmin) return
+
+  if (chat.isGroup) {
+    const groupId = chat.id._serialized
+    const groupInfo = await db.getGroupInfo(groupId)
+
+    await db.updateGroupExceptParticipants(groupId, {
+      auto_invite_link: !groupInfo?.auto_invite_link,
+    })
+
+    if (groupInfo?.auto_invite_link) {
+      await message?.react('🟥')
+    } else {
+      await message?.react('🟩')
+    }
+
+    redis.del(`group-info:${groupId}`)
+
+    return
+  }
+
+  await message?.reply(zap.translateMessage('notgrouperror'))
+}
