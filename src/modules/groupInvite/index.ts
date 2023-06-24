4957385f73a49849874e6dbcbd31099155a1a10a
angelopedroso <solonasusonly0@gmail.com>
2023-06-24 00:19:31 -0300

    feat: added send group invite by command

--- src/modules/groupInvite/index.ts
+++ src/modules/groupInvite/index.ts
@@ -1,14 +1,19 @@
 import { ZapType } from '@modules/zapConstructor'
 import { groupInfoCache } from '@typings/cache/groupInfo.interface'
 
-export async function sendGroupInviteLink(
+export async function sendGroupInviteLink({ message, ...zap }: ZapType) {
+  const chatLink = await zap.getGroupLink()
+  await message?.reply(`https://chat.whatsapp.com/${chatLink}`)
+}
+
+export async function autoGroupInviteLink(
   { message, ...zap }: ZapType,
   groupInfo: groupInfoCache | null | undefined,
 ) {
-  if (groupInfo?.auto_invite_link) {
+  if (groupInfo?.auto_invite_link && !message?.body.includes('!link')) {
     if (message?.body.includes('link')) {
       const chatLink = await zap.getGroupLink()
-      message.reply(`https://chat.whatsapp.com/${chatLink}`)
+      await message.reply(`https://chat.whatsapp.com/${chatLink}`)
     }
   }
 }
