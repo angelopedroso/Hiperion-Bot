6ccc5ec3ebddd90d54f744c6e9fcff04d8572cd6
angelopedroso <solonasusonly0@gmail.com>
2023-06-26 02:08:29 -0300

    feat: created convert stream url to mp3/mp4

---
+++ src/utils/convertStreamToReadableFile.ts
@@ -0,0 +1,41 @@
+import fs from 'fs-extra'
+import ffmpeg from 'fluent-ffmpeg'
+import { MessageMedia } from 'whatsapp-web.js'
+import path from 'path'
+import { getRandomName } from './generateRandomName'
+
+export function convertToMp3(url: string): Promise<MessageMedia> {
+  const filePath = path.resolve(`assets/audio/tmp/${getRandomName('.mp3')}`)
+
+  return new Promise((resolve, reject) => {
+    ffmpeg(url)
+      .format('mp3')
+      .save(filePath)
+      .on('error', function (err) {
+        reject(err)
+      })
+      .on('end', function () {
+        const media = MessageMedia.fromFilePath(filePath)
+        fs.unlinkSync(filePath)
+        resolve(media)
+      })
+  })
+}
+
+export function convertToMp4(url: string): Promise<MessageMedia> {
+  const filePath = path.resolve(`assets/audio/tmp/${getRandomName('.mp4')}`)
+
+  return new Promise((resolve, reject) => {
+    ffmpeg(url)
+      .toFormat('mp4')
+      .save(filePath)
+      .on('end', () => {
+        const media = MessageMedia.fromFilePath(filePath)
+        fs.unlinkSync(filePath)
+        resolve(media)
+      })
+      .on('error', (err) => {
+        reject(err)
+      })
+  })
+}
