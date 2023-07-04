0b58edb33aca4970a94c00ede4528f12e73749a3
angelopedroso <solonasusonly0@gmail.com>
2023-07-03 23:26:57 -0300

    feat: add is bot admin validation on the getinvite

--- src/modules/groupInvite/index.ts
+++ src/modules/groupInvite/index.ts
@@ -2,18 +2,32 @@ import { ZapType } from '@modules/zapConstructor'
 import { groupInfoCache } from '@typings/cache/groupInfo.interface'
 
 export async function sendGroupInviteLink({ message, ...zap }: ZapType) {
+  const botIsAdmin = await zap.isBotAdmin()
+
+  if (!botIsAdmin) {
+    message?.reply(zap.translateMessage('general', 'botisnotadmin'))
+    return
+  }
+
   const chatLink = await zap.getGroupLink()
-  await message?.reply(`https://chat.whatsapp.com/${chatLink}`)
+  message?.reply(`https://chat.whatsapp.com/${chatLink}`)
 }
 
 export async function autoGroupInviteLink(
   { message, ...zap }: ZapType,
   groupInfo: groupInfoCache | null | undefined,
 ) {
+  const botIsAdmin = await zap.isBotAdmin()
+
+  if (!botIsAdmin) {
+    message?.reply(zap.translateMessage('general', 'botisnotadmin'))
+    return
+  }
+
   if (groupInfo?.auto_invite_link && !message?.body.includes('!link')) {
     if (message?.body.includes('link')) {
       const chatLink = await zap.getGroupLink()
-      await message.reply(`https://chat.whatsapp.com/${chatLink}`)
+      message.reply(`https://chat.whatsapp.com/${chatLink}`)
     }
   }
 }
