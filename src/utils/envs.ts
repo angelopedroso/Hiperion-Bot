62dd9242f13f2af02e62a48cb8f12064a4cd5c24
angelopedroso <solonasusonly0@gmail.com>
2023-07-05 14:31:42 -0300

    feat: added ACRCLOUD envs

--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -9,6 +9,10 @@ const API_SIGHTENGINE_SECRET = process.env.API_SIGHTENGINE_SECRET || ''
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ''
 const OPENAI_PASSWORD = process.env.OPENAI_PASSWORD || ''
 
+const ACR_HOST = process.env.ACR_HOST || ''
+const ACR_KEY = process.env.ACR_KEY || ''
+const ACR_SECRET_KEY = process.env.ACR_SECRET_KEY || ''
+
 const DATABASE_URL = process.env.DATABASE_URL || ''
 
 const REDIS_URI = process.env.REDIS_URI || ''
@@ -23,5 +27,8 @@ export {
   LANGUAGE,
   OPENAI_API_KEY,
   OPENAI_PASSWORD,
+  ACR_HOST,
+  ACR_KEY,
+  ACR_SECRET_KEY,
   REDIS_URI,
 }
