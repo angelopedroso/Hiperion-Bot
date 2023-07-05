2fc0b9eda29fd96c4a44d66a1c4d12c91894e4e8
angelopedroso <solonasusonly0@gmail.com>
2023-07-05 14:31:15 -0300

    fix: updated content path

--- src/modules/transcription/index.ts
+++ src/modules/transcription/index.ts
@@ -1,11 +1,10 @@
 import { ZapType } from '@modules/zapConstructor'
 import { MessageTypes } from 'whatsapp-web.js'
 import fs from 'fs-extra'
-import path from 'path'
-import { getRandomName } from '@utils/generateRandomName'
 import { openai } from '@lib/openai'
 import { convertOggToMp3 } from '@utils/convertFile'
 import { printError } from 'cli/terminal'
+import { localPath } from '@utils/globalVariable'
 
 export async function transcriptionAudio({ message, ...zap }: ZapType) {
   const isOwner = await zap.IsOwner()
@@ -18,22 +17,16 @@ export async function transcriptionAudio({ message, ...zap }: ZapType) {
   try {
     if (message?.hasQuotedMsg) {
       const quotedMsg = await message.getQuotedMessage()
-      const localPath = path.join(
-        __dirname,
-        '..',
-        '..',
-        '..',
-        `/assets/audio/tmp/${getRandomName('.ogg')}`,
-      )
+      const path = localPath('audio', 'ogg')
 
       message.react('🔄')
 
       if (quotedMsg.hasMedia && quotedMsg.type === MessageTypes.VOICE) {
         const media = await quotedMsg.downloadMedia()
 
-        await fs.writeFile(localPath, media.data, { encoding: 'base64' })
+        await fs.writeFile(path, media.data, { encoding: 'base64' })
 
-        const file = await convertOggToMp3(localPath)
+        const file = await convertOggToMp3(path)
 
         const { data } = await openai.createTranscription(
           fs.createReadStream(file) as any,
