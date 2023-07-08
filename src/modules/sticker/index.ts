c5bf086c11cd22fa42f4ee7f1508f9be8c93fe1c
angelopedroso <solonasusonly0@gmail.com>
2023-07-08 20:31:15 -0300

    fix: putting trycatch into sticker maker

--- src/modules/sticker/index.ts
+++ src/modules/sticker/index.ts
@@ -13,35 +13,39 @@ const validTypes = [MessageTypes.IMAGE, MessageTypes.VIDEO]
 async function sendSticker({ message, ...zap }: ZapType) {
   const chat = await zap.getChat()
 
-  if (message?.hasMedia && validTypes.includes(message.type)) {
-    const media = await message.downloadMedia()
+  try {
+    if (message?.hasMedia && validTypes.includes(message.type)) {
+      const media = await message.downloadMedia()
 
-    await chat.sendMessage(media, {
-      sendMediaAsSticker: true,
-      stickerAuthor: BOT_NAME,
-      stickerName: zap.translateMessage('fs', 'madeby', { name: BOT_NAME }),
-    })
-
-    return
-  }
-
-  if (message?.hasQuotedMsg) {
-    const quotedMsg = await message.getQuotedMessage()
-
-    if (!quotedMsg.hasMedia) {
-      message.react('⚠')
-      return
-    }
-
-    if (validTypes.includes(quotedMsg.type)) {
-      const media = await quotedMsg.downloadMedia()
-
-      chat.sendMessage(media, {
+      await chat.sendMessage(media, {
         sendMediaAsSticker: true,
         stickerAuthor: BOT_NAME,
         stickerName: zap.translateMessage('fs', 'madeby', { name: BOT_NAME }),
       })
+
+      return
     }
+
+    if (message?.hasQuotedMsg) {
+      const quotedMsg = await message.getQuotedMessage()
+
+      if (!quotedMsg.hasMedia) {
+        message.react('⚠')
+        return
+      }
+
+      if (validTypes.includes(quotedMsg.type)) {
+        const media = await quotedMsg.downloadMedia()
+
+        await chat.sendMessage(media, {
+          sendMediaAsSticker: true,
+          stickerAuthor: BOT_NAME,
+          stickerName: zap.translateMessage('fs', 'madeby', { name: BOT_NAME }),
+        })
+      }
+    }
+  } catch (error: Error | any) {
+    message?.react('⚠')
   }
 }
 
@@ -53,17 +57,21 @@ async function sendAutoSticker(
 
   if (message?.body.includes('!fs') || isViewOnce) return
 
-  if (message?.hasMedia && validTypes.includes(message.type)) {
-    if (groupInfo?.auto_sticker) {
-      const chat = await zap.getChat()
-      const media = await message.downloadMedia()
+  try {
+    if (message?.hasMedia && validTypes.includes(message.type)) {
+      if (groupInfo?.auto_sticker) {
+        const chat = await zap.getChat()
+        const media = await message.downloadMedia()
 
-      await chat.sendMessage(media, {
-        sendMediaAsSticker: true,
-        stickerAuthor: BOT_NAME,
-        stickerName: zap.translateMessage('fs', 'madeby', { name: BOT_NAME }),
-      })
+        await chat.sendMessage(media, {
+          sendMediaAsSticker: true,
+          stickerAuthor: BOT_NAME,
+          stickerName: zap.translateMessage('fs', 'madeby', { name: BOT_NAME }),
+        })
+      }
     }
+  } catch (error: Error | any) {
+    message?.react('⚠')
   }
 }
 
