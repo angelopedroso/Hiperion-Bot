f784959121d3ea801efc54edf3dc62e4746df103
angelopedroso <solonasusonly0@gmail.com>
2023-07-12 16:12:39 -0300

    feat: added createLog when command is called

--- src/bot/modules/modulesWrapper/index.ts
+++ src/bot/modules/modulesWrapper/index.ts
@@ -1,9 +1,12 @@
 import { ZapType } from '@modules/zapConstructor'
 import { commandMap } from './commandMap'
 import { LocaleFileName } from '@locales/@types/command.interface'
+import { db } from '@lib/auth/prisma-query'
 
 export async function registerModules(zap: ZapType) {
   if (zap.message?.body.startsWith('!')) {
+    const { isGroup, name } = await zap.getChat()
+    const { pushname } = await zap.getUser()
     const commandParts = zap.message.body.split(' ')
     const commandName = commandParts[0].replace('!', '').toLowerCase()
     const args = commandParts.slice(1)
@@ -28,14 +31,21 @@ export async function registerModules(zap: ZapType) {
         return
       }
 
-      if (fullArg) {
-        const arg = args.join(' ')
-        await handler(zap, arg)
-        return
-      }
-
       if (expectedArgs === 'any' || args.length === expectedArgs) {
-        await handler(zap, ...args)
+        if (fullArg) {
+          const arg = args.join(' ')
+          await handler(zap, arg)
+        } else {
+          await handler(zap, ...args)
+        }
+
+        await db.createLog({
+          command: commandName,
+          user_name: pushname,
+          is_group: isGroup,
+          chat_name: isGroup ? name : null,
+        })
+
         return
       }
 
