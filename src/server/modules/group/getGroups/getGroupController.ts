b4eefb0aaeb93c7280857d9366c1ab2a9889b26c
angelopedroso <solonasusonly0@gmail.com>
2023-07-19 18:59:51 -0300

    fix: updated getGroups

--- src/server/modules/group/getGroups/getGroupController.ts
+++ src/server/modules/group/getGroups/getGroupController.ts
@@ -1,8 +1,8 @@
+import { db } from '@lib/auth/prisma-query'
 import { Request, Response } from 'express'
-import { getGroups } from './getGroupService'
 
 export async function getGroupsController(_: Request, res: Response) {
-  const allGroups = await getGroups()
+  const allGroups = await db.getAllGroups()
 
   return res.status(200).json(allGroups)
 }
