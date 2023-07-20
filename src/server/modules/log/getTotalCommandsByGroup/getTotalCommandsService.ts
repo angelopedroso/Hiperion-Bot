bccfd5cf8e3dd090d5aed7627b1ffe9418092231
angelopedroso <solonasusonly0@gmail.com>
2023-07-20 19:39:19 -0300

    feat: added chat name prop on total commands

--- src/server/modules/log/getTotalCommandsByGroup/getTotalCommandsService.ts
+++ src/server/modules/log/getTotalCommandsByGroup/getTotalCommandsService.ts
@@ -2,7 +2,7 @@ import { prisma } from '@lib/prisma'
 
 export async function getTotalCommandsService() {
   const log = await prisma.log.groupBy({
-    by: ['groupId'],
+    by: ['groupId', 'chat_name'],
     where: {
       is_group: true,
     },
