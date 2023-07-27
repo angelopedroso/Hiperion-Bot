8e4848f88c2be06d4a0e69ef875a14692dfb9f33
angelopedroso <solonasusonly0@gmail.com>
2023-07-27 17:03:53 -0300

    feat: created getGroupById

--- src/server/routes/group.routes.ts
+++ src/server/routes/group.routes.ts
@@ -1,3 +1,4 @@
+import { getGroupByIdController } from '@server/modules/group/getGroupById/getGroupByIdController'
 import { getGroupsController } from '@server/modules/group/getGroups/getGroupController'
 import { updateGroupController } from '@server/modules/group/updateGroup/updateGroupController'
 import { Router } from 'express'
@@ -5,6 +6,7 @@ import { Router } from 'express'
 const groupRoutes = Router()
 
 groupRoutes.get('/', getGroupsController)
+groupRoutes.get('/:id', getGroupByIdController)
 groupRoutes.put('/:id', updateGroupController)
 
 export { groupRoutes }
