e86023362f04468cb0ce1ede82a58638d0b51ea2
angelopedroso <solonasusonly0@gmail.com>
2023-07-28 23:17:55 -0300

    feat: created removeParticipants request

--- src/server/routes/user.routes.ts
+++ src/server/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { getAdminsController } from '@server/modules/user/getAdmins/getAdminsController'
 import { getUsersController } from '@server/modules/user/getUsers/getUsers/getUsersController'
+import { removeParticipantFromGroupController } from '@server/modules/user/removeParticipantFromGroup/removeParticipantFromGroupController'
 import { Router } from 'express'
 
 const userRoutes = Router()
 
 userRoutes.get('/', getUsersController)
 userRoutes.get('/admin', getAdminsController)
+userRoutes.get('/remove-participants', removeParticipantFromGroupController)
 
 export { userRoutes }
