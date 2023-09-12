015f41ff8f5c2e6a2794ac95efd7d9b043be0578
angelopedroso <solonasusonly0@gmail.com>
2023-09-12 18:58:59 -0300

    feat: added audio profanity detection

--- src/bot/modules/profanityDetector/index.ts
+++ src/bot/modules/profanityDetector/index.ts
@@ -3,6 +3,9 @@ import { db } from '@lib/auth/prisma-query'
 import { printError } from '@cli/terminal'
 import { groupInfoCache } from '@typings/cache/groupInfo.interface'
 import { isProfane } from '@utils/profanity'
+import { MessageTypes } from 'whatsapp-web.js'
+import { localPath } from '@utils/globalVariable'
+import { transcription } from '@utils/transcriptionFunction'
 
 const profanityDetector = async (
   { message, ...zap }: ZapType,
@@ -13,23 +16,35 @@ const profanityDetector = async (
 
     const isAdmin = await zap.getUserIsAdmin(user.id._serialized)
 
-    if (!isAdmin) {
+    if (!isAdmin && groupInfo?.anti_profane) {
       const chat = await zap.getChat()
 
       const msgProfanity = isProfane(message?.body)
+      const isBotAdmin = await zap.isBotAdmin()
 
-      if (groupInfo?.anti_profane) {
-        const isBotAdmin = await zap.isBotAdmin()
+      if (!isBotAdmin) {
+        await db.updateGroupExceptParticipants(chat.id._serialized, {
+          anti_profane: false,
+        })
 
-        if (!isBotAdmin) {
-          await db.updateGroupExceptParticipants(chat.id._serialized, {
-            anti_profane: false,
-          })
+        return
+      }
 
-          return
-        }
+      if (msgProfanity) {
+        await message?.delete(true)
 
-        if (msgProfanity) {
+        return
+      }
+
+      if (message?.type === MessageTypes.VOICE) {
+        const media = await message.downloadMedia()
+        const path = localPath('audio', 'ogg')
+
+        const messageTranscripted = await transcription({ media, path })
+
+        const msgProfanityTranscripted = isProfane(messageTranscripted)
+
+        if (msgProfanityTranscripted) {
           await message?.delete(true)
         }
       }
